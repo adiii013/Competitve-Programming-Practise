ae283ee65b77f4387a77646a185b422638d355eb
Aditya <adidada023@gmail.com>
2021-12-22 09:51:09 +0530

    6

---
+++ 2]Codechef/6_The_One_Where_It_All_Began_S01E01.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+
+typedef long long ll;
+
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int n;cin>>n;
+  while(n--){
+      int a;cin>>a;
+      if(a>6) cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
+  }
+
+return 0;
+}
