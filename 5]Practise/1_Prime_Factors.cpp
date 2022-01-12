8b6e7da260fba1c573d5085291b881c020b5b38b
Aditya <adidada023@gmail.com>
2022-01-12 18:20:31 +0530

    1

---
+++ 5]Practise/1_Prime_Factors.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(a) (a).begin(),(a).end()
+
+typedef long long ll;
+
+void PrimeFactors(int n){
+    while(n%2==0){
+        cout<<2<<" ";
+        n = n/2;
+    }
+    for(int i = 3;i<=sqrt(n);i = i+2){
+        while(n%i==0){
+            cout<<i<<" ";
+            n = n/i;
+        }
+    }
+    if(n>2) cout<<n<<endl;
+}
+
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+
+PrimeFactors(t);
+
+return 0;
+}
