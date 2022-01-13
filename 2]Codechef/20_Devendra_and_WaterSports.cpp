75abc962fb5d6594d91f3ed83df9c667e92ea91d
Aditya <adidada023@gmail.com>
2022-01-13 10:15:29 +0530

    20

---
+++ 2]Codechef/20_Devendra_and_WaterSports.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b) for(int i = a;i<b;i++)
+#define pi pair<int,int>
+#define all(a) (a).begin(),(a).end()
+
+typedef long long ll;
+
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b,c,d,e;cin>>a>>b>>c>>d>>e;
+  int sum = a-b;
+  int check = c+d+e;
+  if(sum>=check) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+}
+
+return 0;
+}
