2a87be4ed732313268a1fb6024eb08cf951f452f
Aditya <adidada023@gmail.com>
2022-01-19 21:00:21 +0530

    38

---
+++ 1]Codeforce/38_Luntik_and_Concerts.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b,c) for(int a = b;a<c;a++)
+#define pi pair<int,int>
+#define all(a) (a).begin(),(a).end()
+
+typedef long long ll;
+
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b,c;cin>>a>>b>>c;
+  ll sum = a+c;
+  cout<<sum%2<<endl;
+}
+
+return 0;
+}
