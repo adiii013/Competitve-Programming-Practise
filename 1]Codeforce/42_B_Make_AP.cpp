d965992102e0e7d0c6a0207b1d712231e9aa66f6
Aditya <adidada023@gmail.com>
2022-01-21 19:59:36 +0530

    42

---
+++ 1]Codeforce/42_B_Make_AP.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int,int>
+#define rep(a,b,c) for(int a = b;a<c;a++)
+#define pi pair<int,int>
+#define all(a) (a).begin(),(a).end()
+
+typedef long long ll;
+
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a, b, c;
+    cin >> a >> b >> c;
+
+    int new_a = b - (c - b);
+    if(new_a >= a && new_a % a == 0 && new_a != 0) {
+        cout << "YES\n";
+        break;
+    }
+    int new_b = a + (c - a)/2;
+    if(new_b >= b && (c-a)%2 == 0 && new_b % b == 0 && new_b != 0) {
+        cout << "YES\n";
+        break;
+    }
+    int new_c = a + 2*(b - a);
+    if(new_c >= c && new_c % c == 0 && new_c != 0) {
+        cout << "YES\n";
+        break;
+    }
+    cout << "NO\n";
+}
+
+return 0;
+}
