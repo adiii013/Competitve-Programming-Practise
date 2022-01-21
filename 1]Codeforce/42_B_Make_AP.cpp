09adaf4d7c8578311a85001a7df618ea0f84d0e7
Aditya <adidada023@gmail.com>
2022-01-21 20:04:12 +0530

    42

--- 1]Codeforce/42_B_Make_AP.cpp
+++ 1]Codeforce/42_B_Make_AP.cpp
@@ -20,19 +20,23 @@ while(t--){
     int new_a = b - (c - b);
     if(new_a >= a && new_a % a == 0 && new_a != 0) {
         cout << "YES\n";
-        break;
+        continue;
     }
+
     int new_b = a + (c - a)/2;
     if(new_b >= b && (c-a)%2 == 0 && new_b % b == 0 && new_b != 0) {
         cout << "YES\n";
-        break;
+        continue;
     }
+
     int new_c = a + 2*(b - a);
     if(new_c >= c && new_c % c == 0 && new_c != 0) {
         cout << "YES\n";
-        break;
+        continue;
     }
+
     cout << "NO\n";
+    
 }
 
 return 0;
