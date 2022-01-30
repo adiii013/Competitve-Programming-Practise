ed06f889fc44d2a2e4fbdb972908013688a0ecb8
Aditya <adidada023@gmail.com>
2022-01-30 11:17:15 +0530

    63

---
+++ 1]Codeforce/Set 2/63_Maximum_Cost_Deletion.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n, a, b;
+    string s;
+    cin >> n >> a >> b >> s;
+    int m = unique(s.begin(), s.end()) - s.begin();
+    cout << n * a + max(n * b, (m / 2 + 1) * b) << '\n';
+  
+}
+return 0;
+}
