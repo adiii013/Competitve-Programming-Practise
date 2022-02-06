2477f4b7d88001ace4030b40275e18464ff65eef
Aditya <adidada023@gmail.com>
2022-02-06 16:56:39 +0530

    81

---
+++ 1]Codeforce/Set 2/81_B_Fun_With_Even_Subarray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+    int n;cin >> n;
+    vector<int> a(n+1);
+    for(int i=1; i<=n; i++)
+        cin >> a[i];
+
+    vector<int> b = a;
+    reverse(b.begin()+1,b.end());
+
+    int ans = 0, x = 1;
+
+    while( x < n ){
+        if( b[x+1] == b[1] ){
+            x ++;
+            continue;
+        }
+        ans ++;
+        x *= 2;
+    }
+        cout << ans << '\n';
+}
+return 0;
+}
