7ebd14429c8bb442a9552af7131ff0ae1df1049e
Aditya <adidada023@gmail.com>
2022-02-10 12:34:37 +0530

    86

---
+++ 1]Codeforce/Set 2/86_A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  ll n;cin>>n;
+  cout<<1-n<<" "<<n<<"\n";
+}
+return 0;
+}
