2b019a893de6ea5207d4811b2b4b93b849f974e6
Aditya <adidada023@gmail.com>
2022-03-07 23:49:00 +0530

    119

---
+++ 1]Codeforce/Set 2/119_B_Quality_vs_Quantity.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  ll n;cin>>n;
+  vector<ll> v(n);
+  for(ll i = 0;i<n;i++) cin>>v[i];
+  sort(v.begin(),v.end());
+  
+  if(v[n-1]>v[0]+v[1]) cout<<"YES\n";
+  else cout<<"NO\n";
+}
+return 0;
+}
