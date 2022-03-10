520a855272906b3d8e9dde7e148ff2c0ac234f62
Aditya <adidada023@gmail.com>
2022-03-10 21:11:36 +0530

    123

---
+++ 1]Codeforce/Set 2/123_A_Playoff.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+int t;cin>>t;
+while(t--){
+  int n;cin>>n;
+  int total = pow(2,n);
+  cout<<total-1<<endl;
+}
+return 0;
