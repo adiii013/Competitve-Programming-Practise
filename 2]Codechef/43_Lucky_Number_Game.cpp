0aa0c6db2ba1c6878bc1819f1c3d08b9b19dff08
Aditya <adidada023@gmail.com>
2022-03-26 23:45:17 +0530

    43

---
+++ 2]Codechef/43_Lucky_Number_Game.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n,a,b;cin>>n>>a>>b;
+  int ali = 0,bob = 0, albo = 0;
+  for(int i = 0;i<n;i++){
+      int d;cin>>d;
+      if(d%a==0 && d%b==0) albo++;
+      else if(d%a==0) bob++;
+      else if(d%b==0) ali++;
+  }
+  if(albo>0) bob++;
+  if(bob>ali) cout<<"BOB\n";
+  else cout<<"ALICE\n";
+}
+return 0;
+}
