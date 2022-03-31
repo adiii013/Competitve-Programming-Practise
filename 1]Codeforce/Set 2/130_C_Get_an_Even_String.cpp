aae14c01ce505ccf16370bec2343d84b3c2339b9
Aditya <adidada023@gmail.com>
2022-03-31 23:36:05 +0530

    130

---
+++ 1]Codeforce/Set 2/130_C_Get_an_Even_String.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  string s;cin>>s;
+  vector<int> v;
+  bool arr[s.length()];
+  for(int i = 0;i<)
+}
+return 0;
+}
