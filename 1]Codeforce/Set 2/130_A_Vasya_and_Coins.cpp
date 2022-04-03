06d03a5df24b2d0a87038add5a8226bb531006f8
Aditya <adidada023@gmail.com>
2022-04-03 13:20:19 +0530

    130

--- 1]Codeforce/Set 2/130_A_Vasya_and_Coins.cpp
+++ 1]Codeforce/Set 2/130_A_Vasya_and_Coins.cpp
@@ -9,11 +9,12 @@ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 int t;cin>>t;
 while(t--){
   int a,b;cin>>a>>b;
+  
   if(a==0){
       cout<<1<<"\n";
       continue;
   }
-  
+
   cout<< a+2*b+1<<"\n";
 }
 
