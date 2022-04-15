c83647b10f4a0b3ef333e87a3ee4324509d2dcad
Aditya <adidada023@gmail.com>
2022-04-15 17:53:09 +0530

    131

---
+++ 1]Codeforce/Set 2/131_A_GCD_vs_LCM.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int n;cin>>n;
+  cout<<n-3<<" "<<1<<" "<<1<<" "<<1<<"\n";
+}
+return 0;
+}
